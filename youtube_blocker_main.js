03a58c1c1ff13cdb8a3d4020c8badcb7d988f045
David <73255970+Shelicus@users.noreply.github.com>
2022-02-08 14:26:40 +0100

    update Kommentare

--- youtube_blocker_main.js
+++ youtube_blocker_main.js
@@ -16,20 +16,27 @@
     'use strict';
     var intvl, intvl2, intvl3, startToAppend = 4000, loop = 5000, loopse = 500;
     //startToAppend -> Time for wait till YouTube classes load;
+   
+    //legt neues Element an im Script
     setTimeout(appendElement, startToAppend);
+    
+    //Aufruf der Funktion 1
     intvl = setInterval(function(){
         removeEndScreens("ytp-ce-element");
     },loop);
 
+    //Aufruf der Funktion 2
     intvl2 = setInterval(function(){
         removevideos("div#items.style-scope.ytd-watch-next-secondary-results-renderer");
     },loopse);
 
+    //Aufruf der Funktion 3
     intvl3 = setInterval(function(){
         removevideosstartseite("contents");
     },loopse);
     //loop = check interval for new End-Screens;
 
+    //Anlegen eines Neuen Elements -> Eigentlich nicht mehr von nöten
     function appendElement() {
         var el = document.createElement("span");
         el.id = "removed_ess";
@@ -37,6 +44,7 @@
         document.getElementsByClassName("style-scope ytd-video-secondary-info-renderer")[1].appendChild(el);
     }
 
+    //Funktion zu löschung der Verlinkten Videos am Ende des Video
     function removeEndScreens(className){
         var elements = document.getElementsByClassName(className);
         while(elements.length > 0){
@@ -49,15 +57,16 @@
         }
     }
 
+    //Funktion zu löscung der Videos Unterhalb des Videos
     function removevideos(Name){
         var elemento;
         elemento = document.querySelectorAll(Name)
         if(elemento.length > 0){
-//             console.log(elemento);
              elemento[0].remove();
         }
      }
 
+    //Funktion zur löscung der Videos auf der Startseite
     function removevideosstartseite(ClassName){
         var elementozwe;
         elementozwe = document.querySelectorAll('div#contents[class="style-scope ytd-rich-grid-renderer"]')
